6a8eafde47c85d8f7b43b4a1ed121e3f6c8ac193
BraelynJae <jae.braelyn@gmail.com>
2025-03-17 20:23:00 -0500

    Attempt Safari Menu Fix
    - Attempt to fix mobile Safari menu by moving css and src lines to index.html file

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,8 +8,6 @@ function Footer() {
       <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <span class="block text-sm text-gray-400 text-center">Thank you for using <Link to="/" class="hover:underline">PsychRoadMap</Link>. <Link to="/admin" class="hover:underline">Admin</Link></span>
       </div>
-      <script src="https://flowbite.com/docs/flowbite.min.js?v=3.1.2a"></script>
-      <script src="https://flowbite.com/docs/docs.js?v=3.1.2a"></script>
     </footer>
   );
 };
