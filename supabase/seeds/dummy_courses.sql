d83afc95c43a5cf86f7f7b6b199a2d62eb0192ea
William Behrens <Quantum.b4@gmail.com>
2025-03-19 12:17:30 -0500

    sync seed data with current courses

--- supabase/seeds/dummy_courses.sql
+++
@@ -1,5 +0,0 @@
-insert into "public"."Course"
-  ("department", "course_code", "course_cluster", "course_name")
-values
-  ('PSY', 10000, 'PSY Core', 'Principles of Psychology'),
-  ('PSY', 15700, 'Elective', 'Psychology of Leadership');
