4bdd195a84f9aa1a1b7700d4a4f20569e873345d
William Behrens <Quantum.b4@gmail.com>
2025-03-20 10:52:06 -0500

    add cors headers

--- supabase/functions/course-info/index.ts
+++ supabase/functions/course-info/index.ts
@@ -1,8 +1,14 @@
 // Setup type definitions for built-in Supabase Runtime APIs
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { createClient } from "jsr:@supabase/supabase-js@2";
+import { corsHeaders } from '../_shared/cors.ts'
 
 Deno.serve(async (req) => {
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+
   try {
     const supabase = createClient(
       Deno.env.get("SUPABASE_URL") ?? "",
@@ -15,10 +21,10 @@ Deno.serve(async (req) => {
     }
 
     return new Response(JSON.stringify({ data }), {
-      headers: { "Content-Type": "application/json" },
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
       status: 200,
     });
   } catch (err) {
-    return new Response(String(err?.message ?? err), { status: 500 });
+    return new Response(String(err?.message ?? err), { headers: corsHeaders, status: 500 });
   }
 });
