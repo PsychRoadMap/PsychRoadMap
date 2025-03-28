1e973acd4227921765dd065f46f2fa8293ce1400
William Behrens <Quantum.b4@gmail.com>
2025-03-28 01:57:02 -0500

    possibly use db functions for filtering

--- supabase/seeds/initial_ratings.sql
+++ supabase/seeds/initial_ratings.sql
@@ -1,4 +1,17 @@
 INSERT INTO "public"."Mastery" ("mastery_name", "mastery_domain") VALUES
 	("Psychology", "Knowledge"),
-    ("Writing", ""),
-    ("", "");
+    ("Writing", "Skills"),
+    ("Science", "Skills");
+
+INSERT INTO "public"."Develops" ("department", "course_code", "mastery_name", "mastery_domain", "lv_rating") VALUES
+	("PSY", 15700, "Psychology", "Knowledge", 71),
+	("PSY", 15700, "Writing", "Skills", 70),
+	("PSY", 15700, "Science", "Skills", 50),
+ 
+    ("PSY", 18100, "Psychology", "Knowledge", 30),
+    ("PSY", 18100, "Writing", "Skills", 70),
+    ("PSY", 18100, "Science", "Skills", 0),
+
+    ("PSY", 39012, "Psychology", "Knowledge", 58),
+    ("PSY", 39012, "Writing", "Skills", 30),
+    ("PSY", 39012, "Science", "Skills", 0);
