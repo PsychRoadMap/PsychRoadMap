4507ee7eab6a131416fc9f8503a66e25c084250c
William Behrens <Quantum.b4@gmail.com>
2025-03-31 22:15:33 -0500

    add first unit test

--- supabase/functions/course-info/index.ts
+++ supabase/functions/course-info/index.ts
@@ -4,7 +4,6 @@ import { createClient } from "jsr:@supabase/supabase-js@2";
 import { serveCors } from "../_shared/cors.ts";
 
 const COURSE_TABLE = "Course";
-const DEVELOPS_TABLE = "Develops";
 
 async function courseInfo(req: Request): Promise<Response> {
   const supabase = createClient(
@@ -19,34 +18,30 @@ async function courseInfo(req: Request): Promise<Response> {
 
   try {
     // We're going to try and populate this
-    let courseData, courseError;
+    // let courseData, courseError;
+    let courseQuery;
 
     if (filter) {
       console.log(`Filtering courses for '${filter}'`);
 
-      // We only need the FK/PKs from the Develops table as we just need to know if the course has been rated 
-      // Currently all filters require knowledge of course ratings so we will always need to pull this data
-      const { developsData, developsError } = await supabase.from(DEVELOPS_TABLE).select("department", "course_code");
-      if (developsError) {
-        throw developsError;
-      }
-
       // Change the query based on filtering method, probably a better way to do this
       switch (filter) {
         case "rated":
-          { courseData, courseError } = await supabase.from(COURSE_TABLE).select("*").filter("department", "in", developsData);
+          courseQuery = await supabase.rpc("select_rated_courses");
           break;
         default:
           throw Error("Unknown filtering method.");
       }
-
-      // Make sure everything worked
-      if (courseError) {
-        throw courseError;
-      }
+    } else {
+      courseQuery = await supabase.from(COURSE_TABLE).select("*");
     }
 
-    return new Response(JSON.stringify({ data: courseData }), {
+    // Make sure everything worked
+    if (courseQuery.error) {
+      throw courseQuery.error;
+    }
+
+    return new Response(JSON.stringify(courseQuery.data), {
       headers: { "Content-Type": "application/json" },
       status: 200,
     });
